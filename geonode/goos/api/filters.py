3baf66128127e8eca91af315a7090a5b446790e8
Pieter Provoost <pieterprovoost@gmail.com>
2021-12-01 15:06:17 +0100

    fixes https://github.com/iobis/bioeco-app/issues/5

--- geonode/goos/api/filters.py
+++ geonode/goos/api/filters.py
@@ -8,5 +8,5 @@ class EovFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         if request.query_params.get("eovs"):
             eov_ids = [eid for eid in request.query_params.get("eovs").split(",")]
-            queryset = queryset.filter(eovs__in=eov_ids)
+            queryset = queryset.filter(eovs__in=eov_ids).distinct()
         return queryset
