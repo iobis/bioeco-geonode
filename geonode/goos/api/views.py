f1c63b5349fb303d20ed385219883e8a185c2952
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-16 07:52:08 +0800

    Delete geonode directory

--- geonode/goos/api/views.py
+++
@@ -1,8 +0,0 @@
-from geonode.goos.models import Eov
-from rest_framework import viewsets
-from geonode.goos.api.serializers import EovSerializer
-
-
-class EovViewSet(viewsets.ModelViewSet):
-    queryset = Eov.objects.all()
-    serializer_class = EovSerializer
