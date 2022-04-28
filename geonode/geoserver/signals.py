2bd4bc1a4d3d65c6111f601d32d75868ccb3ae78
root <pieterprovoost@gmail.com>
2022-04-28 14:16:49 +0000

    added outputs

--- geonode/geoserver/signals.py
+++ geonode/geoserver/signals.py
@@ -79,6 +79,7 @@ def geoserver_post_save(instance, sender, created, **kwargs):
     # so we should select what will be handled here
     if isinstance(instance, Layer):
         instance.sops = None
+        instance.outputs = None
         instance_dict = model_to_dict(instance)
         payload = json_serializer_producer(instance_dict)
         try:
