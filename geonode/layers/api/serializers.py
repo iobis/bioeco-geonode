8644a715d535705de801f49b2ff4f3e002a594cc
root <pieterprovoost@gmail.com>
2022-04-28 11:52:27 +0000

    added sops to api response

--- geonode/layers/api/serializers.py
+++ geonode/layers/api/serializers.py
@@ -98,7 +98,7 @@ class LayerSerializer(ResourceBaseSerializer):
             'pk', 'uuid', 'name', 'workspace', 'store', 'storeType', 'charset',
             'is_mosaic', 'has_time', 'has_elevation', 'time_regex', 'elevation_regex',
             'use_featureinfo_custom_template', 'featureinfo_custom_template',
-            'default_style', 'styles', 'attribute_set', 'url', 'tkeywords'
+            'default_style', 'styles', 'attribute_set', 'url', 'tkeywords', 'sops'
         )
 
     name = serializers.CharField(read_only=True)
